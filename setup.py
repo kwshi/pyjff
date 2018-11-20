abdc0db810a2db4d8195579bf24756be47c75a37
Kye W. Shi <shi.kye@gmail.com>
2018-11-20 10:31:59 -0800

    create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='pyjff',
+    version='0.0.0',
+    author='Kye W. Shi',
+    author_email='kwshi@hmc.edu',
+    url='https://github.com/kwshi/pyjff',
+    license='MIT',
+    description='Python library for reading and executing JFLAP .jff automata',
+)
